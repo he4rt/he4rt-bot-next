94c265f02b3fa6debda87e9f52b1ffb76d925877
Novout <novout@hotmail.com>
2022-11-17 00:25:35 -0300

    chore: commands register

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,41 +1,28 @@
 import { Events, Routes } from 'discord.js'
-import { Context } from '../types'
+import { Command, Context, He4rtClient } from '../types'
 import { useAnnounce } from './announce'
 import { useCommandsList } from './commands_list'
 import { useIntroduce } from './introduce'
 
-export const registerCommands = async ({ client, rest }: Context) => {
-  const [commandsData, commandsCallback] = useCommandsList()
-  const [introduceData, introduceCallback] = useIntroduce()
-  const [announceData, announceCallback] = useAnnounce()
+const registerHooks = (client: He4rtClient, commands: Command[]) => {
+  commands.forEach((command) => {
+    client.commands.set(command[0], command[1])
+  })
+}
 
-  client.commands.set(commandsData, commandsCallback)
-  client.commands.set(introduceData, introduceCallback)
-  client.commands.set(announceData, announceCallback)
+export const registerCommands = async ({ client, rest }: Context) => {
+  registerHooks(client, [useCommandsList(), useIntroduce(), useAnnounce()])
 
   client.on(Events.InteractionCreate, async (interaction) => {
     if (!interaction.isChatInputCommand()) return
 
-    switch (interaction.commandName) {
-      case introduceData.name:
-        introduceCallback(interaction, client)
-        break
-      case announceData.name:
-        announceCallback(interaction, client)
-        break
-      case commandsData.name:
-        commandsCallback(interaction, client)
-        break
-      default:
-        await interaction.reply({
-          content: 'Comando inválido!',
-          ephemeral: true,
-        })
+    for (const [key, cb] of client.commands) {
+      if (key.name === interaction.commandName) cb && (await cb(interaction, client))
     }
   })
 
   await rest.put(
     Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID as string, process.env.DISCORD_GUILD_ID as string),
-    { body: [...client.commands.keys()] }
+    { body: client.commands.keys() }
   )
 }
