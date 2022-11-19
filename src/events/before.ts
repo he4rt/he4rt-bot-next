caa69c587adf132100ff3e3fb1bd54c68b486ed7
Novout <novout@hotmail.com>
2022-11-19 11:22:55 -0300

    chore: ignore gamification listener

--- src/events/before.ts
+++ src/events/before.ts
@@ -1,12 +1,14 @@
 import { Events } from 'discord.js'
 import { commandsListener } from '@/commands'
 import { He4rtClient } from '@/types'
-import { gamificationListener } from './gamification'
 
 export const beforeListeners = (client: He4rtClient) => {
+  /*
+   * TODO: fds essa merda bugada do caralho  
   client.on(Events.MessageCreate, async (message) => {
     await gamificationListener(client, message)
   })
+  */
 
   client.on(Events.InteractionCreate, async (interaction) => {
     if (interaction.isChatInputCommand()) await commandsListener(client, interaction)
