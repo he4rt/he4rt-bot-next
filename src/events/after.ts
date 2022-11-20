84cc73036b138d8dbdd39accd5e0763af3e5faf2
Novout <novout@hotmail.com>
2022-11-20 01:36:47 -0300

    feat(presence): multiple messages

--- src/events/after.ts
+++ src/events/after.ts
@@ -2,13 +2,35 @@ import { ActivityType } from 'discord.js'
 import { He4rtClient } from '@/types'
 
 export const setPresence = (client: He4rtClient) => {
+  const type = ActivityType.Watching as Exclude<ActivityType, ActivityType.Custom>
+
   client?.user?.setPresence({
     status: 'online',
     activities: [
       {
+        type,
+        url: 'https://heartdevs.com',
+        name: 'da comunidade para a comunidade.',
+      },
+      {
+        type,
+        url: 'https://beacons.ai/heartdevs',
+        name: 'beacons.ai/heartdevs',
+      },
+      {
+        type,
+        url: 'https://heartdevs.com',
         name: 'heartdevs.com',
-        type: ActivityType.Watching,
-        url: 'https://heartdevs.com/',
+      },
+      {
+        type,
+        url: 'https://github.com/he4rt',
+        name: 'github.com/he4rt',
+      },
+      {
+        type,
+        url: 'https://heartdevs.com',
+        name: '/apresentar',
       },
     ],
   })
