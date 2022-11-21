ae221c2c9392962f92faa304dc7398cbf9c67c2c
Novout <novout@hotmail.com>
2022-11-21 15:29:11 -0300

    fix(events)!: break embed cases

--- src/events/channel.ts
+++ src/events/channel.ts
@@ -5,9 +5,9 @@ import { isAdministrator } from '@/utils'
 export const suppressEmbedMessagesInBusyChannels = async (message: Message) => {
   const validChannels = [CHAT_CHANNEL, MEETING_CHANNEL, MEETING_DELAS_CHANNEL]
 
-  if (validChannels.some((v) => v.id !== message.channel.id)) return
+  if (validChannels.some((v) => v.id === message.channel.id)) {
+    if (isAdministrator(message.member) || message.embeds.length === 0) return
 
-  if (isAdministrator(message.member) || message.embeds.length === 0) return
-
-  await message.suppressEmbeds(true).catch(() => {})
+    await message.suppressEmbeds(true).catch(() => {})
+  }
 }
