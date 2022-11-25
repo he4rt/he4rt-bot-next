f922479358189c5b4ea27bceafbcaff65671b5f1
Novout <novout@hotmail.com>
2022-11-25 20:19:20 -0300

    fix(cron): awaitable providers

--- src/events/cron.ts
+++ src/events/cron.ts
@@ -14,45 +14,43 @@ export const verifyApoiaseMembers = async (client: He4rtClient) => {
   await new CronJob('00 00 12 10 * *', () => {
     guild.members
       .fetch()
-      .then((members) => {
+      .then(async (members) => {
         const normalize = members.filter((t) => isApoiaseMember(t) && !isNitroBoosterMember(t))
 
         for (const [_, member] of normalize) {
           // apoia.se requests by second limit
-          js()
-            .sleep(1000)
-            .then(() => {
-              client.api.he4rt
-                .users(member.id)
-                .get<UserGET>()
-                .then(({ email }) => {
-                  client.api.apoiase.backers
-                    .charges(email)
-                    .get<ApoiaseGET>()
-                    .then(({ isBacker, isPaidThisMonth, thisMonthPaidValue }) => {
-                      if (
-                        isBacker &&
-                        isPaidThisMonth &&
-                        thisMonthPaidValue &&
-                        thisMonthPaidValue >= APOIASE_CUSTOM_COLOR_MINIMAL_VALUE
-                      ) {
-                        return
-                      }
+          await js().sleep(2000)
 
-                      client.api.he4rt
-                        .users(member.id)
-                        .put<UserPUT>({
-                          is_donator: 0,
-                        })
-                        .then(async () => {
-                          await member.roles.remove(DONATOR_ROLE.id).catch(() => {})
-                        })
-                        .catch(() => {})
+          client.api.he4rt
+            .users(member.id)
+            .get<UserGET>()
+            .then(({ email }) => {
+              client.api.apoiase.backers
+                .charges(email)
+                .get<ApoiaseGET>()
+                .then(({ isBacker, isPaidThisMonth, thisMonthPaidValue }) => {
+                  if (
+                    isBacker &&
+                    isPaidThisMonth &&
+                    thisMonthPaidValue &&
+                    thisMonthPaidValue >= APOIASE_CUSTOM_COLOR_MINIMAL_VALUE
+                  ) {
+                    return
+                  }
+
+                  client.api.he4rt
+                    .users(member.id)
+                    .put<UserPUT>({
+                      is_donator: 0,
+                    })
+                    .then(async () => {
+                      await member.roles.remove(DONATOR_ROLE.id).catch(() => {})
                     })
                     .catch(() => {})
                 })
                 .catch(() => {})
             })
+            .catch(() => {})
         }
       })
       .catch(() => {})
