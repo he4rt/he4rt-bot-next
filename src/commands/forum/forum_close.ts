234ab01d411cfe93cd568d732f274cfd10d31e13
Giovane Cardoso <novout@hotmail.com>
2022-12-08 13:05:08 -0300

    refactor(forum): mark moderator to actions in thread channel

--- src/commands/forum/forum_close.ts
+++ src/commands/forum/forum_close.ts
@@ -1,6 +1,7 @@
-import { GuildMember, PermissionFlagsBits, SlashCommandBuilder } from 'discord.js'
+import { GuildMember, SlashCommandBuilder, ThreadChannel } from 'discord.js'
 import { Command } from '@/types'
 import { FORUM_CLOSE } from '@/defines/commands.json'
+import { MODERATION_ROLE } from '@/defines/ids.json'
 import { CHANNEL_OPTION } from '-/commands/forum_close.json'
 import { getForumChannel, getTargetMember, reply } from '@/utils'
 
@@ -9,21 +10,31 @@ export const useForumClose = (): Command => {
     .setName(FORUM_CLOSE.TITLE)
     .setDescription(FORUM_CLOSE.DESCRIPTION)
     .setDMPermission(false)
-    .addChannelOption((option) => option.setName('canal').setDescription(CHANNEL_OPTION).setRequired(true))
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageThreads)
 
   return [
     data,
     async (interaction, client) => {
-      const target = interaction.options.get('canal')
-
       try {
         const channel = await getForumChannel(client)
         const { threads } = await channel.threads.fetch()
 
-        const thread = threads.get(target.value as string)
-        await thread.edit({ locked: true, archived: true })
-        thread.appliedTags = []
+        const target = interaction.channel as ThreadChannel
+
+        if (!target.isThread()) {
+          await reply(interaction).error()
+
+          return
+        }
+
+        const thread = threads.get(target.id)
+
+        await thread.send(
+          `<@&${MODERATION_ROLE.id}> o usuário <@${interaction.user.id}> está requisitando o fechamento deste post!`
+        )
+
+        await reply(interaction).success()
+
+        await thread.edit({ archived: true, locked: true })
 
         client.logger.emit({
           message: `O canal de ajuda **${thread.name}** foi fechado por ${getTargetMember(
@@ -32,8 +43,6 @@ export const useForumClose = (): Command => {
           type: 'command',
           color: 'success',
         })
-
-        await reply(interaction).success()
       } catch (e) {
         await reply(interaction).error()
       }
