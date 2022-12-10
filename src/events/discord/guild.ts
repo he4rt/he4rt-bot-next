13ad439de19ff72ef62fc9098d501c2d265ec9fe
Giovane Cardoso <novout@hotmail.com>
2022-12-10 19:51:37 -0300

    feat(events): send message to a new user in guild

--- src/events/discord/guild.ts
+++ src/events/discord/guild.ts
@@ -1,6 +1,28 @@
 import { He4rtClient, IntroducePOST } from '@/types'
 import { getTargetMember } from '@/utils'
 import { GuildMember, PartialGuildMember } from 'discord.js'
+import { INITIAL_MESSAGE } from '-/events/guild_enter.json'
+
+export const sendDmToNewUser = (client: He4rtClient, member: GuildMember) => {
+  member
+    .createDM()
+    .then(async (dm) => {
+      await dm.send(INITIAL_MESSAGE).catch(() => {
+        client.logger.emit({
+          message: `Não foi possível enviar a mensagem de boas-vindas na DM para o usuário ${getTargetMember(member)}!`,
+          type: 'bot',
+          color: 'error',
+        })
+      })
+    })
+    .catch(() => {
+      client.logger.emit({
+        message: `Não foi possível enviar a mensagem de boas-vindas na DM para o usuário ${getTargetMember(member)}!`,
+        type: 'bot',
+        color: 'error',
+      })
+    })
+}
 
 export const deletePossibleUserInServerLeave = (client: He4rtClient, member: GuildMember | PartialGuildMember) => {
   client.api.he4rt
