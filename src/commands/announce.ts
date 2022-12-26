2b5134b248d692934a00ba4b418a98aa2c8c756c
Giovane Cardoso <novout@hotmail.com>
2022-12-26 14:04:56 -0300

    refactor(announce): text send instead embed

--- src/commands/announce.ts
+++ src/commands/announce.ts
@@ -2,8 +2,8 @@ import { PermissionFlagsBits, SlashCommandBuilder } from 'discord.js'
 import { Command } from '@/types'
 import { ANNOUNCE } from '@/defines/commands.json'
 import { ADVERTS_CHANNEL } from '@/defines/ids.json'
-import { TEXT_OPTION, IMAGE_OPTION, EMBED_CONTENT, EMBED_TITLE } from '-/commands/announce.json'
-import { embedTemplate, getChannel, reply } from '@/utils'
+import { TEXT_OPTION, EMBED_CONTENT } from '-/commands/announce.json'
+import { getChannel, reply } from '@/utils'
 
 export const useAnnounce = (): Command => {
   const data = new SlashCommandBuilder()
@@ -11,24 +11,16 @@ export const useAnnounce = (): Command => {
     .setDescription(ANNOUNCE.DESCRIPTION)
     .setDMPermission(false)
     .addStringOption((option) => option.setName('texto').setDescription(TEXT_OPTION).setRequired(true))
-    .addStringOption((option) => option.setName('imagem').setDescription(IMAGE_OPTION))
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
 
   return [
     data,
     async (interaction, client) => {
       const text = interaction.options.get('texto')
-      const image = interaction.options.get('imagem')
-
-      const embed = embedTemplate({
-        title: EMBED_TITLE,
-        description: text!.value as string,
-      })
-      if (image?.value) embed.setImage(image.value as string)
 
       const channel = getChannel({ id: ADVERTS_CHANNEL.id, client })
 
-      await channel?.send({ content: EMBED_CONTENT, embeds: [embed] })
+      await channel?.send({ content: `${EMBED_CONTENT} ${text.value as string}` })
 
       await reply(interaction).success()
     },
