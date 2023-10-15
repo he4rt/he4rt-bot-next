93454870a275bc4c939e7d5d3e72ec6e0e7f6695
Anibal <anibalsolon@gmail.com>
2023-10-15 16:01:42 -0500

    enh: add sticky message to intro channel w button

--- src/commands/introduction.ts
+++ src/commands/introduction.ts
@@ -1,5 +1,5 @@
-import { CommandInteraction, DMChannel, GuildMember, SlashCommandBuilder, Message } from 'discord.js'
-import { Cancellable, Command, IntroducePOST, IntroducePUT, RoleDefine, UserGETBody } from '@/types'
+import { CommandInteraction, DMChannel, GuildMember, SlashCommandBuilder, Message, ButtonInteraction, TextChannel, ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js'
+import { Cancellable, Command, He4rtClient, IntroducePOST, RoleDefine, UserGETBody } from '@/types'
 import {
   PRESENTATIONS_CHANNEL,
   PRESENTED_ROLE,
@@ -35,7 +35,7 @@ const removePresentingFlag = async (member: GuildMember) => {
   await member.roles.remove(PRESENTING_ROLE.id)
 }
 
-const nextTextMessage = async (dm: DMChannel, interaction: CommandInteraction): Promise<Cancellable<string>> => {
+const nextTextMessage = async (dm: DMChannel, interaction: CommandInteraction | ButtonInteraction): Promise<Cancellable<string>> => {
   try {
     const result = await dm.awaitMessages({
       filter: (m) => m.author.id === interaction.user.id,
@@ -54,7 +54,7 @@ const nextMultipleRoleSelection = async (
   text: string,
   dm: DMChannel,
   member: GuildMember,
-  interaction: CommandInteraction
+  interaction: CommandInteraction | ButtonInteraction
 ): Promise<number | boolean> => {
   await dm.send(text)
   await dm.send(
@@ -77,7 +77,7 @@ const nextMultipleRoleSelection = async (
   }
 }
 
-const nextUFSelection = async (dm: DMChannel, interaction: CommandInteraction): Promise<string | false> => {
+const nextUFSelection = async (dm: DMChannel, interaction: CommandInteraction | ButtonInteraction): Promise<string | false> => {
   await dm.send(
     VALID_PRESENTATION_RF.reduce(
       (acc, val, index) => (acc += `**${index + 1}**` + ` -   ${val.id} ${val.name}` + '\n'),
@@ -101,7 +101,7 @@ const nextRoleSelection = async (
   text: string,
   dm: DMChannel,
   member: GuildMember,
-  interaction: CommandInteraction
+  interaction: CommandInteraction | ButtonInteraction
 ): Promise<number | false> => {
   await dm.send(text)
   await dm.send(roles.reduce((acc, val, index) => (acc += index + 1 + ` -   ${val.emoji} ${val.name}` + '\n'), '\n'))
@@ -124,7 +124,7 @@ const nextRoleSelection = async (
 const nextHe4rtDelasRole = async (
   dm: DMChannel,
   member: GuildMember,
-  interaction: CommandInteraction
+  interaction: CommandInteraction | ButtonInteraction
 ): Promise<number | false> => {
   const roles: RoleDefine[] = [HE4RT_DELAS_ROLE]
 
@@ -145,7 +145,7 @@ const nextHe4rtDelasRole = async (
   return value
 }
 
-const validateAccess = async (dm: DMChannel, interaction: CommandInteraction): Promise<boolean> => {
+const validateAccess = async (dm: DMChannel, interaction: CommandInteraction | ButtonInteraction): Promise<boolean> => {
   await sendInDM(dm, interaction, INTRODUCTION.CONTINUE)
 
   if (!sendInDM) return false
@@ -155,7 +155,7 @@ const validateAccess = async (dm: DMChannel, interaction: CommandInteraction): P
   return true
 }
 
-const nextStringsData = async (dm: DMChannel, interaction: CommandInteraction): Promise<UserGETBody | false> => {
+const nextStringsData = async (dm: DMChannel, interaction: CommandInteraction | ButtonInteraction): Promise<UserGETBody | false> => {
   await dm.send(INTRODUCTION.USER.NAME)
   const name = await nextTextMessage(dm, interaction)
   if (isCancellable(name)) return false
@@ -197,6 +197,205 @@ const nextStringsData = async (dm: DMChannel, interaction: CommandInteraction): 
   }
 }
 
+const ensureStickyIntroductionMessage = async (client: He4rtClient) => {
+  const channel = getChannel<TextChannel>({ id: PRESENTATIONS_CHANNEL.id, client })
+
+  const messages = await channel.messages.fetchPinned()
+
+  for (const [_, message] of messages) {
+    await message.delete().catch(() => {})
+  }
+
+  const component = new ActionRowBuilder<ButtonBuilder>()
+    .addComponents(
+      new ButtonBuilder()
+        .setCustomId('c-introduce')
+        .setLabel(INTRODUCTION.STICKY.CALL_TO_ACTION)
+        .setStyle(ButtonStyle.Primary)
+    )
+
+  const embed = embedTemplate({
+    title: INTRODUCTION.STICKY.TITLE,
+    description: INTRODUCTION.STICKY.DESCRIPTION,
+    target: {
+      user: client.user,
+      icon: true,
+    }
+  })
+
+  try {
+    const msg = await channel.send({
+      embeds: [embed],
+      components: [component],
+    })
+
+    await msg.pin()
+
+  } catch (e) {
+  }
+}
+
+const startIntroductionFlow = async (client: He4rtClient, interaction: CommandInteraction | ButtonInteraction): Promise<void> => {
+  const author = interaction.user
+  const member = interaction.member as GuildMember
+
+  try {
+    if (isPresentingMember(member)) {
+      await reply(interaction).errorPresentingFail()
+
+      return
+    }
+
+    const dm = await client.users.createDM(author);
+
+    const valid = await validateAccess(dm, interaction)
+
+    if (!valid) return
+
+    await setPresentingFlag(member)
+
+    const body = await nextStringsData(dm, interaction)
+
+    if (body === false) {
+      await dm.send(INTRODUCTION.STOP)
+      return
+    }
+
+    const multipleRoles = await nextMultipleRoleSelection(
+      VALID_PRESENTATION_DEV_ROLES,
+      INTRODUCTION.USER.LANGUAGES,
+      dm,
+      member,
+      interaction
+    )
+
+    if (multipleRoles === false) {
+      await dm.send(INTRODUCTION.STOP)
+      return
+    }
+
+    const role = await nextRoleSelection(
+      VALID_PRESENTATION_ENG_ROLES,
+      INTRODUCTION.USER.ENGLISH,
+      dm,
+      member,
+      interaction
+    )
+
+    if (role === false) {
+      await dm.send(INTRODUCTION.STOP)
+      return
+    }
+
+    const delas = await nextHe4rtDelasRole(dm, member, interaction)
+
+    if (delas === false) {
+      await dm.send(INTRODUCTION.STOP)
+      return
+    }
+
+    const embed = embedTemplate({
+      title: `${INTRODUCTION.EMBED.TITLE}${author.username}`,
+      target: {
+        user: author,
+        icon: true,
+      },
+      delas: delas === 1,
+      fields: [
+        [
+          { name: INTRODUCTION.EMBED.NAME, value: body.info.name, inline: true },
+          { name: INTRODUCTION.EMBED.NICKNAME, value: body.info.nickname, inline: true },
+          { name: INTRODUCTION.EMBED.ABOUT, value: body.info.about, inline: true },
+        ],
+        [
+          { name: INTRODUCTION.EMBED.GIT, value: body.info.github_url, inline: true },
+          { name: INTRODUCTION.EMBED.LINKEDIN, value: body.info.linkedin_url, inline: true },
+          {
+            name: INTRODUCTION.EMBED.LANGUAGES,
+            value: validDisplayDevRoles(member),
+          },
+          {
+            name: INTRODUCTION.EMBED.ENGLISH,
+            value: validDisplayEngRoles(member),
+            inline: true,
+          },
+        ],
+      ],
+    })
+
+    const channel = getChannel({ id: PRESENTATIONS_CHANNEL.id, client })
+
+    await channel
+      .send({
+        content: `👋 <@${interaction.user.id}>!`,
+        embeds: [embed],
+      })
+      .then(async (msg: Message) => {
+        await msg.react(delas === 1 ? HE4RT_DELAS_EMOJI_ID : HE4RT_EMOJI_ID).catch(async () => {
+          await msg.react('💜').catch(() => {})
+        })
+      })
+
+    await member.roles.add(PRESENTED_ROLE.id)
+    await removePresentingFlag(member)
+
+    await ensureStickyIntroductionMessage(client)
+
+    client.api.he4rt.providers.discord
+      .post<IntroducePOST>({
+        provider_id: member.id,
+        username: `${member?.nickname ?? member.user.username}-${member.user.discriminator}`,
+      })
+      .then(() => {
+        client.api.he4rt.users
+          .profile(member.id)
+          .put<IntroducePOST>(body)
+          .then(() => {
+            client.logger.emit({
+              type: 'http',
+              color: 'info',
+              message: `${getTargetMember(member)} apresentou e teve a sua conta criada!`,
+              user: member.user,
+            })
+          })
+          .catch((e) => {
+            client.logger.emit({
+              type: 'http',
+              color: 'error',
+              message: `${getTargetMember(member)} não consegiu criar sua conta! Erro: ${e}`,
+              user: member.user,
+            })
+          })
+      })
+      .catch(() => {
+        client.api.he4rt.users
+          .profile(member.id)
+          .put<IntroducePOST>(body)
+          .then(() => {
+            client.logger.emit({
+              type: 'http',
+              color: 'info',
+              message: `${getTargetMember(member)} apresentou e teve a sua conta alterada!`,
+              user: member.user,
+            })
+          })
+          .catch((e) => {
+            client.logger.emit({
+              type: 'http',
+              color: 'error',
+              message: `${getTargetMember(member)} não consegiu criar sua conta! Erro: ${e}`,
+              user: member.user,
+            })
+          })
+      })
+
+    await dm.send(INTRODUCTION.FINISH)
+  } catch {
+  } finally {
+    await removePresentingFlag(member)
+  }
+}
+
 export const useIntroduction = (): Command => {
   const data = new SlashCommandBuilder()
     .setName(INTRODUCE.TITLE)
@@ -205,152 +404,12 @@ export const useIntroduction = (): Command => {
 
   return [
     data,
-    async (interaction, client) => {
-      const author = interaction.user
-      const member = interaction.member as GuildMember
-
-      if (isPresentingMember(member)) {
-        await reply(interaction).errorPresentingFail()
-
-        return
-      }
-
-      client.users
-        .createDM(author)
-        .then(async (dm) => {
-          const valid = await validateAccess(dm, interaction)
-
-          if (!valid) return
-
-          await setPresentingFlag(member)
-
-          const body = await nextStringsData(dm, interaction)
-
-          if (body === false) return await dm.send(INTRODUCTION.STOP)
-
-          const multipleRoles = await nextMultipleRoleSelection(
-            VALID_PRESENTATION_DEV_ROLES,
-            INTRODUCTION.USER.LANGUAGES,
-            dm,
-            member,
-            interaction
-          )
-
-          if (multipleRoles === false) return await dm.send(INTRODUCTION.STOP)
-
-          const role = await nextRoleSelection(
-            VALID_PRESENTATION_ENG_ROLES,
-            INTRODUCTION.USER.ENGLISH,
-            dm,
-            member,
-            interaction
-          )
-
-          if (role === false) return await dm.send(INTRODUCTION.STOP)
-
-          const delas = await nextHe4rtDelasRole(dm, member, interaction)
-
-          if (delas === false) return await dm.send(INTRODUCTION.STOP)
-
-          const embed = embedTemplate({
-            title: `${INTRODUCTION.EMBED.TITLE}${author.username}`,
-            target: {
-              user: author,
-              icon: true,
-            },
-            delas: delas === 1,
-            fields: [
-              [
-                { name: INTRODUCTION.EMBED.NAME, value: body.info.name, inline: true },
-                { name: INTRODUCTION.EMBED.NICKNAME, value: body.info.nickname, inline: true },
-                { name: INTRODUCTION.EMBED.ABOUT, value: body.info.about, inline: true },
-              ],
-              [
-                { name: INTRODUCTION.EMBED.GIT, value: body.info.github_url, inline: true },
-                { name: INTRODUCTION.EMBED.LINKEDIN, value: body.info.linkedin_url, inline: true },
-                {
-                  name: INTRODUCTION.EMBED.LANGUAGES,
-                  value: validDisplayDevRoles(member),
-                },
-                {
-                  name: INTRODUCTION.EMBED.ENGLISH,
-                  value: validDisplayEngRoles(member),
-                  inline: true,
-                },
-              ],
-            ],
-          })
-
-          const channel = getChannel({ id: PRESENTATIONS_CHANNEL.id, client })
-
-          await channel
-            .send({
-              content: `👋 <@${interaction.user.id}>!`,
-              embeds: [embed],
-            })
-            .then(async (msg: Message) => {
-              await msg.react(delas === 1 ? HE4RT_DELAS_EMOJI_ID : HE4RT_EMOJI_ID).catch(async () => {
-                await msg.react('💜').catch(() => {})
-              })
-            })
-
-          await member.roles.add(PRESENTED_ROLE.id)
-          await removePresentingFlag(member)
-
-          client.api.he4rt.providers.discord
-            .post<IntroducePOST>({
-              provider_id: member.id,
-              username: `${member?.nickname ?? member.user.username}-${member.user.discriminator}`,
-            })
-            .then(() => {
-              client.api.he4rt.users
-                .profile(member.id)
-                .put<IntroducePOST>(body)
-                .then(() => {
-                  client.logger.emit({
-                    type: 'http',
-                    color: 'info',
-                    message: `${getTargetMember(member)} apresentou e teve a sua conta criada!`,
-                    user: member.user,
-                  })
-                })
-                .catch((e) => {
-                  client.logger.emit({
-                    type: 'http',
-                    color: 'error',
-                    message: `${getTargetMember(member)} não consegiu criar sua conta! Erro: ${e}`,
-                    user: member.user,
-                  })
-                })
-            })
-            .catch(() => {
-              client.api.he4rt.users
-                .profile(member.id)
-                .put<IntroducePOST>(body)
-                .then(() => {
-                  client.logger.emit({
-                    type: 'http',
-                    color: 'info',
-                    message: `${getTargetMember(member)} apresentou e teve a sua conta alterada!`,
-                    user: member.user,
-                  })
-                })
-                .catch((e) => {
-                  client.logger.emit({
-                    type: 'http',
-                    color: 'error',
-                    message: `${getTargetMember(member)} não consegiu criar sua conta! Erro: ${e}`,
-                    user: member.user,
-                  })
-                })
-            })
-
-          await dm.send(INTRODUCTION.FINISH)
-        })
-        .catch(() => {})
-        .finally(async () => {
-          await removePresentingFlag(member)
-        })
-    },
+    async (interaction, client) => startIntroductionFlow(client, interaction),
   ]
+}
+
+export const resolveIntroduceCommandButtonEvents = async (client: He4rtClient, interaction: ButtonInteraction) => {
+  if (interaction.customId === 'c-introduce') {
+    await startIntroductionFlow(client, interaction)
+  }
 }
