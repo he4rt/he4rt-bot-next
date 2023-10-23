ee637e082e548683e1d5e8db5486abe7b5e9dbbc
Canhassi <arthurcanhassi@gmail.com>
2023-10-23 19:03:21 -0300

    feat: add some messages before update room name

--- src/events/cron/breakfast.ts
+++ src/events/cron/breakfast.ts
@@ -2,7 +2,7 @@ import { He4rtClient } from '@/types'
 import { getChannel, getGuild, getTaggedMembers } from '@/utils'
 import { DYNAMIC_CATEGORY_CHANNEL } from '@/defines/ids.json'
 import { CronJob } from 'cron'
-import { CategoryChannel, ChannelType } from 'discord.js'
+import { CategoryChannel, ChannelType, range } from 'discord.js'
 
 export const manageBreakfast = async (client: He4rtClient) => {
   const guild = getGuild(client)
@@ -23,11 +23,21 @@ export const manageBreakfast = async (client: He4rtClient) => {
       userLimit: 50,
     })
 
+    const phrases = [
+      'A alegria dura uma noite mas as contas vem pela manhã, bora trabaia!',
+      'O sol nasce, a bicicleta anda o lobo uiva e o urso panda!',
+      'É galera bora trabalhar e aguardar o Al-Mosso, alias já pode? https://www.jahpodealmossar.com.br/',
+      'Todos podem vencer menos os seus boletos, bora trabalhar!!',
+      'Nem só de café vive o homi, e as conta fi?'
+    ];
+
+    const indexPharseDay = Math.floor(Math.random() * phrases.length)
+
     setTimeout(async () => {
-      await voice.send(`${getTaggedMembers(voice.members.map((m) => m.id))} O canal de voz será deletado daqui um minuto!`).catch(() => {})
+      await voice.send(`${getTaggedMembers(voice.members.map((m) => m.id))} ${phrases[indexPharseDay]}`).catch(() => {})
 
       setTimeout(async () => {
-        await voice.delete().catch(() => {})
+        await voice.edit({name: "Trabalho"}).catch(() => {})
       }, 1000 * 60)
     }, 1000 * 60 * 60 * 5)
   }).start()
